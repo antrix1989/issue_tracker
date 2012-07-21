9087a85556d562b310ea52fef58ab8ebe9ab3e4d
Sergey Demchenko <antrix1989@gmail.com>
2012-07-21 09:50:40 +0300

    Added server validation of the ticket. Assigned reference to a ticket.

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,12 @@
 class Ticket < ActiveRecord::Base
   attr_accessible :assigned_to_id, :body, :customer_email, :customer_name, :reference, :status_id, :subject
+  
+  validates :customer_name, :customer_email, :subject, :body, :presence => true
+  
+  after_save :assign_reference
+  
+  private
+    def assign_reference
+      self.update_column(:reference, "UKR-%06d" % self.id)
+    end
 end
