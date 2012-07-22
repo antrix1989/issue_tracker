a2ab3f47a371da13c9c10ad529feed42c13ee3b7
Sergey Demchenko <antrix1989@gmail.com>
2012-07-22 21:28:16 +0300

    Removed Comments controller. Deny acces to search page.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,6 @@
 class SearchController < ApplicationController
+  before_filter :authenticate_user!
+  
   def index
     # Quick jump to a ticket.
     if params[:search].match(/^UKR-(\d+)$/)
--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,6 @@ IssueTraker::Application.routes.draw do
   get "welcome/index"
   root :to => "welcome#index"
 
-  resources :comments
-
   resources :tickets
 
   # The priority is based upon order of creation:
